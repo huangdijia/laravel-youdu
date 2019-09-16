e27d53b7868f7350ff1c7d34847cfdaf5a2f022d
huangdijia <huangdijia@gmail.com>
2019-09-16 15:57:19 +0800

    拆分代码 增加部门、用户、群管理模块（待完善）

--- src/User.php
+++ src/User.php
@@ -54,6 +54,167 @@ class User
     }
 
     /**
+     * 创建用户
+     *
+     * @param integer $userId 用户id(帐号)，企业内必须唯一。长度为1~64个字符（包括汉字和英文字母）
+     * @param string $name 用户名称。长度为0~64个字符（包括汉字和英文字母，可为空）
+     * @param integer $gender 性别，整型。0表示男性，1表示女性
+     * @param string $mobile 手机号码。企业内必须唯一
+     * @param string $phone 电话号码
+     * @param string $email 邮箱。长度为0~64个字符
+     * @param array $dept 所属部门列表,不超过20个
+     * @return bool
+     */
+    public function create(int $userId, string $name, int $gender = 0, string $mobile = '', string $phone = '', string $email = '', array $dept = [])
+    {
+        $parameters = $this->youdu->encryptMsg(json_encode([
+            "buin"   => $this->youdu->getBuin(),
+            "appId"  => $this->youdu->getAppId(),
+            "userId" => $userId,
+            "name"   => $name,
+            "gender" => $gender,
+            "mobile" => $mobile,
+            "phone"  => $phone,
+            "email"  => $email,
+            "dept"   => $dept,
+        ]));
+
+        $resp = HttpClient::post($this->youdu->url('/cgi/user/create'), $parameters);
+
+        if ($resp['httpCode'] != 200) {
+            throw new \Exception("http request code " . $resp['httpCode'], ErrorCode::$IllegalHttpReq);
+        }
+
+        $body = json_decode($resp['body'], true);
+
+        if ($body['errcode'] !== 0) {
+            throw new \Exception($body['errmsg'], $body['errcode']);
+        }
+
+        return true;
+    }
+
+    /**
+     * 更新用户
+     *
+     * @param integer $userId 用户id(帐号)，企业内必须唯一。长度为1~64个字符（包括汉字和英文字母）
+     * @param string $name 用户名称。长度为0~64个字符（包括汉字和英文字母，可为空）
+     * @param integer $gender 性别，整型。0表示男性，1表示女性
+     * @param string $mobile 手机号码。企业内必须唯一
+     * @param string $phone 电话号码
+     * @param string $email 邮箱。长度为0~64个字符
+     * @param array $dept 所属部门列表,不超过20个
+     * @return bool
+     */
+    public function update(int $userId, string $name, int $gender = 0, string $mobile = '', string $phone = '', string $email = '', array $dept = [])
+    {
+        $parameters = $this->youdu->encryptMsg(json_encode([
+            "buin"   => $this->youdu->getBuin(),
+            "appId"  => $this->youdu->getAppId(),
+            "userId" => $userId,
+            "name"   => $name,
+            "gender" => $gender,
+            "mobile" => $mobile,
+            "phone"  => $phone,
+            "email"  => $email,
+            "dept"   => $dept,
+        ]));
+
+        $resp = HttpClient::post($this->youdu->url('/cgi/user/update'), $parameters);
+
+        if ($resp['httpCode'] != 200) {
+            throw new \Exception("http request code " . $resp['httpCode'], ErrorCode::$IllegalHttpReq);
+        }
+
+        $body = json_decode($resp['body'], true);
+
+        if ($body['errcode'] !== 0) {
+            throw new \Exception($body['errmsg'], $body['errcode']);
+        }
+
+        return true;
+    }
+
+    /**
+     * 更新职位信息
+     *
+     * @param int $userId 用户id(帐号)，企业内必须唯一。长度为1~64个字符（包括汉字和英文字母）
+     * @param integer $deptId 部门Id。用户必须在该部门内
+     * @param string $position 职务
+     * @param integer $weight 职务权重。用户拥有多个职务时，权重值越大的职务排序越靠前
+     * @param integer $sortId 用户在部门中的排序，值越大排序越靠前
+     * @return bool
+     */
+    public function updatePosition(int $userId, int $deptId, string $position = '', int $weight = 0, int $sortId = 0)
+    {
+        $parameters = $this->youdu->encryptMsg(json_encode([
+            "buin"     => $this->youdu->getBuin(),
+            "appId"    => $this->youdu->getAppId(),
+            "userId"   => $userId,
+            "deptId"   => $deptId,
+            "position" => $position,
+            "weight"   => $weight,
+            "sortId"   => $sortId,
+        ]));
+
+        $resp = HttpClient::post($this->youdu->url('/cgi/user/positionupdate'), $parameters);
+
+        if ($resp['httpCode'] != 200) {
+            throw new \Exception("http request code " . $resp['httpCode'], ErrorCode::$IllegalHttpReq);
+        }
+
+        $body = json_decode($resp['body'], true);
+
+        if ($body['errcode'] !== 0) {
+            throw new \Exception($body['errmsg'], $body['errcode']);
+        }
+
+        return true;
+    }
+
+    /**
+     * 删除用户
+     *
+     * @param int|array $userId
+     * @return bool
+     */
+    public function delete($userId)
+    {
+        // batch delete
+        if (is_array($userId)) {
+            $parameters = $this->youdu->encryptMsg(json_encode([
+                "buin"    => $this->youdu->getBuin(),
+                "appId"   => $this->youdu->getAppId(),
+                "delList" => $userId,
+            ]));
+
+            $resp = HttpClient::post($this->youdu->url('/cgi/user/batchdelete'), $parameters);
+
+            if ($resp['httpCode'] != 200) {
+                throw new \Exception("http request code " . $resp['httpCode'], ErrorCode::$IllegalHttpReq);
+            }
+
+            $body = json_decode($resp['body'], true);
+
+            if ($body['errcode'] !== 0) {
+                throw new \Exception($body['errmsg'], $body['errcode']);
+            }
+
+            return true;
+        }
+
+        // single delete
+        $resp    = HttpClient::get($this->youdu->url('/cgi/user/delete'), ['userId' => $userId]);
+        $decoded = json_decode($resp['body'], true);
+
+        if ($decoded['errcode'] !== 0) {
+            throw new \Exception($decoded['errmsg'], 1);
+        }
+
+        return true;
+    }
+
+    /**
      * 用户详情
      *
      * @param integer $userId
