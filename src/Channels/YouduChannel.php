c1c82fa6ee42b5bb2d226e48d6cc9688382c9d1d
huangdijia <huangdijia@gmail.com>
2019-11-21 10:36:36 +0800

    fix

--- src/Channels/YouduChannel.php
+++ src/Channels/YouduChannel.php
@@ -19,7 +19,7 @@ class YouduChannel implements Channel
     public function send($notifiable, Notification $notification)
     {
         $message = $notification->toYoudu($notifiable);
-        $app     = is_callable([$notification, 'app']) ? $notification->app() : '';
+        $app     = is_callable([$notification, 'app']) ? $notification->app($notifiable) : '';
 
         if (
             !($to = $notifiable->routeNotificationFor('youdu', $notification))
