a4f8debb11ed0dae6aec829c2c2fd7623d88e9c3
hdj <hdj@addcn.com>
2020-06-30 12:01:20 +0800

    增加异常监听器，支持本地化

--- src/Listeners/ExceptionListener.php
+++ src/Listeners/ExceptionListener.php
@@ -6,6 +6,7 @@ use Huangdijia\Youdu\Notifications\TextNotification;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Log\Events\MessageLogged;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Str;
 use Throwable;
 
 class ExceptionListener
@@ -36,58 +37,20 @@ class ExceptionListener
             return;
         }
 
-        $e = $event->context['exception'];
-
-        // $message = sprintf(
-        //     "【%s Exception Reporting】\n\nEnvironment: %s\n%sUrl: %s\nException: %s\nMessage: %s\nPosition: %s:%s\nTime: %s\n\n",
-        //     config('app.name'),
-        //     config('app.env'),
-        //     $this->getCurrentBranch(),
-        //     request()->fullUrl(),
-        //     get_class($e),
-        //     $e->getMessage(),
-        //     $e->getFile(),
-        //     $e->getLine(),
-        //     now()->toDateTimeString()
-        // );
-
-        $messages = [
-            __('youdu.environment') => config('app.env'),
-            __('youdu.branch')      => $this->getCurrentBranch(),
-            __('youdu.url')         => request()->fullUrl(),
-            __('youdu.exception')   => get_class($e),
-            __('youdu.message')     => $e->getMessage(),
-            __('youdu.position')    => $e->getFile() . ':' . $e->getLine(),
-            __('youdu.time')        => now()->toDateTimeString(),
-        ];
-
-        $message = sprintf(
-            "[%s %s %s]\n\n%s",
+        $message = Str::start($this->assembleMessage($event->context['exception'], app()->runningInConsole()), sprintf(
+            "[%s %s %s]\n\n",
             config('app.name'),
             __('youdu.exception'),
-            __('youdu.reporting'),
-            collect($messages)
-                ->transform(function ($item, $key) {
-                    if ($item == '') {
-                        return '';
-                    }
-
-                    return "{$key}: {$item}";
-                })
-                ->filter()
-                ->join("\n")
-        );
+            __('youdu.reporting')
+        ));
 
         try {
             collect(config('youdu.exception.receivers', []))
                 ->transform(function ($route) {
                     return Notification::route('youdu', $route);
                 })
-                ->tap(function ($notifiables) use ($message) {
+                ->whenNotEmpty(function ($notifiables) use ($message) {
                     /** @var Collection $notifiables */
-                    if ($notifiables->isEmpty()) {
-                        return;
-                    }
 
                     if (config('youdu.exception.report_now')) {
                         Notification::sendNow($notifiables, new TextNotification($message, config('youdu.exception.report_app', 'default')));
@@ -107,7 +70,7 @@ class ExceptionListener
     }
 
     /**
-     * 获取当前分支名
+     * Get git current branch name
      * @return string
      * @throws BindingResolutionException
      */
@@ -125,5 +88,35 @@ class ExceptionListener
         $currentBranch = trim(substr($headContent, 16));
 
         return $currentBranch;
+    }
+
+    /**
+     * Assemble message
+     * @param Throwable $e
+     * @param bool $runningInConsole
+     * @return mixed
+     * @throws BindingResolutionException
+     * @throws UnitException
+     */
+    protected function assembleMessage(Throwable $e, $runningInConsole = false)
+    {
+        return collect()
+            ->put(__('youdu.environment'), config('app.env'))
+            ->when($this->getCurrentBranch(), function ($collection, $branch) {
+                /** @var \Illuminate\Support\Collection $collection */
+                $collection->put(__('youdu.branch'), $branch);
+            })
+            ->when(!$runningInConsole, function ($collection) {
+                /** @var \Illuminate\Support\Collection $collection */
+                $collection->put(__('youdu.url'), app('request')->fullUrl());
+            })
+            ->put(__('youdu.exception'), get_class($e))
+            ->put(__('youdu.message'), $e->getMessage())
+            ->put(__('youdu.position'), $e->getFile() . ':' . $e->getLine())
+            ->put(__('youdu.time'), date('Y-m-d H:i:s'))
+            ->transform(function ($value, $key) {
+                return sprintf('%s: %s', $key, $value);
+            })
+            ->join("\n");
     }
 }
